819c1c879b4fe920ac6e3dbb1295e62468708c94
augusto <augusto.fariasdosantos@gmail.com>
2023-08-08 16:32:44 -0300

    exxx

--- ERP_UC13/sql/inserts.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO login_user VALUES(NULL, 'Augusto', '0123');
-INSERT INTO login_user VALUES(NULL, 'Beatriz', '4567');
-INSERT INTO login_user VALUES(NULL, 'Carlos', '89ab');
-INSERT INTO login_user VALUES(NULL, 'Débora', 'cdef');
